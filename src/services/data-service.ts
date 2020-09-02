bde4c8012b0d1b413eebee6c4b798af083cce681
ellankz <enelipa@gmail.com>
2020-09-02 09:41:53 +0300

    feat: Create functions to all and single item for each data type

--- src/services/data-service.ts
+++ src/services/data-service.ts
@@ -1,4 +1,12 @@
-import { User } from '../models/data-models';
+import {
+  User,
+  Task,
+  TaskScore,
+  CrossCheckSession,
+  ReviewRequest,
+  Review,
+  Dispute,
+} from '../models/data-models';
 
 class DataService {
   baseURL: string;
@@ -20,8 +28,72 @@ class DataService {
     return this.getResource<User[]>('/users');
   }
 
-  async getSingleUser(id: string): Promise<User> {
-    const result = await this.getResource<User[]>(`/users?githubId=${id}`);
+  async getSingleUser(githubId: string): Promise<User> {
+    const result = await this.getResource<User[]>(
+      `/users?githubId=${githubId}`
+    );
+    return result[0];
+  }
+
+  getAllTasks(): Promise<Task[]> {
+    return this.getResource<Task[]>('/tasks');
+  }
+
+  async getSingleTask(id: string): Promise<Task> {
+    const result = await this.getResource<Task[]>(`/tasks?id=${id}`);
+    return result[0];
+  }
+
+  getAllTaskScores(): Promise<TaskScore[]> {
+    return this.getResource<TaskScore[]>('/taskScores');
+  }
+
+  async getSingleTaskScore(taskId: string): Promise<TaskScore> {
+    const result = await this.getResource<TaskScore[]>(
+      `/taskScores?task=${taskId}`
+    );
+    return result[0];
+  }
+
+  getAllCrossCheckSessions(): Promise<CrossCheckSession[]> {
+    return this.getResource<CrossCheckSession[]>('/crossCheckSessions');
+  }
+
+  async getSingleCrossCheckSession(id: string): Promise<CrossCheckSession> {
+    const result = await this.getResource<CrossCheckSession[]>(
+      `/crossCheckSessions?id=${id}`
+    );
+    return result[0];
+  }
+
+  getAllReviewRequests(): Promise<ReviewRequest[]> {
+    return this.getResource<ReviewRequest[]>('/reviewRequests');
+  }
+
+  async getSingleReviewRequest(id: string): Promise<ReviewRequest> {
+    const result = await this.getResource<ReviewRequest[]>(
+      `/reviewRequests?id=${id}`
+    );
+    return result[0];
+  }
+
+  getAllReviews(): Promise<Review[]> {
+    return this.getResource<Review[]>('/reviews');
+  }
+
+  async getSingleReview(id: string): Promise<Review> {
+    const result = await this.getResource<Review[]>(`/reviews?id=${id}`);
+    return result[0];
+  }
+
+  getAllDisputes(): Promise<Dispute[]> {
+    return this.getResource<Dispute[]>('/disputes');
+  }
+
+  async getSingleDispute(reviewId: string): Promise<Dispute> {
+    const result = await this.getResource<Dispute[]>(
+      `/disputes?reviewId=${reviewId}`
+    );
     return result[0];
   }
 }
