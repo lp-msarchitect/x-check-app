4a762b51f1551a12effe7e934ea8029767a7fa57
ellankz <enelipa@gmail.com>
2020-09-02 14:55:12 +0300

    feat: add role picker for testing purposes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,42 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Students from '../Students/Students';
 import Home from '../Home/Home';
 import './App.css';
+import { UserRole } from '../../models/data-models';
 
 const App = (): JSX.Element => {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/students">Students</Link>
-            </li>
-            <li>
-              <Link to="/users">Students</Link>
-            </li>
-          </ul>
-        </nav>
+  const [role, setRole] = useState<UserRole>('student');
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/students">
-            <Students />
-          </Route>
-          <Route path="/users">
-            <Students />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+  const handleRoleChange = (
+    event: React.FormEvent<HTMLSelectElement>
+  ): void => {
+    setRole(event.currentTarget.value as UserRole);
+  };
+
+  return (
+    <div>
+      <div>
+        <select value={role} onChange={handleRoleChange}>
+          <option value="author">Author</option>
+          <option value="student">Student</option>
+          <option value="supervisor">Supervisor</option>
+          <option value="coursemanager">Course Manager</option>
+        </select>
       </div>
-    </Router>
+      <p>Your current role is: {role}</p>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/students">Students</Link>
+              </li>
+              <li>
+                <Link to="/users">Students</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/students">
+              <Students />
+            </Route>
+            <Route path="/users">
+              <Students />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 };
 
