059de7e545833d4794111d27a4d525de7e5d5258
Leonid Petrov <leonid.petrov@latoken.com>
2020-09-06 13:58:51 +0300

    feat: redirect to home page after log in

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Spin, Button } from 'antd';
 import { GithubOutlined } from '@ant-design/icons';
+import { Redirect } from 'react-router-dom';
 
 const clientId = '140ee27ef8df8ece846a';
 const proxyUrl = 'https://x-check-app.herokuapp.com/authenticate/';
 
 const Login = (): JSX.Element => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     const url = window.location.href;
     const hasCode = url.includes('?code=');
@@ -28,6 +30,7 @@ const Login = (): JSX.Element => {
             .then((userData) => {
               localStorage.setItem('isLoggedIn', JSON.stringify(true));
               localStorage.setItem('githubId', JSON.stringify(userData.login));
+              setIsLoggedIn(true);
               setIsLoading(false);
             });
         });
@@ -36,6 +39,7 @@ const Login = (): JSX.Element => {
 
   return (
     <>
+      {isLoggedIn ? <Redirect to="/" /> : null}
       {isLoading ? (
         <Spin />
       ) : (
