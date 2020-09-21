cf300d78631a99c7b31ab9d9884a0ca850fbdb8e
goplomax11 <62140181+goplomax11@users.noreply.github.com>
2020-09-21 18:45:16 +0300

    add Collapse

--- src/components/Sessions/Sessions.tsx
+++ src/components/Sessions/Sessions.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { Collapse } from 'antd';
+import { AnyAction } from 'redux';
+import {
+  AppReduxState,
+  ReviewsState,
+  TasksState,
+} from '../../models/redux-models';
+import { useDispatch, useSelector } from 'react-redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { getReviews, getTasks } from '../../actions';
+
+const { Panel } = Collapse;
+type AppDispatch = ThunkDispatch<ReviewsState, void, AnyAction>;
 
 const Sessions = (): JSX.Element => {
+
+
+
+
+  const dispatch: AppDispatch = useDispatch();
+
+  const tasks = useSelector<AppReduxState, TasksState>((state) => state.tasks);
+
+  useEffect(() => {
+    dispatch(getReviews());
+    dispatch(getTasks());
+  }, [dispatch]);
+
+  function callback(key:any) {
+    console.log(key);
+  }
+
   return (
     <div>
-      <ul>
-        <li>Session</li>
-      </ul>
+    <Collapse  onChange={callback}>
+
+      <Panel header="This is panel header 1" key='1'>
+        <p>das</p>
+      </Panel>
+      
+    </Collapse>,
     </div>
   );
 };
