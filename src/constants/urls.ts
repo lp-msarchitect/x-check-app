1386f42e771afde59a485825f735b9ef37156761
Leonid Petrov <leonid.petrov@latoken.com>
2020-09-27 21:24:02 +0300

    sone fixes

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,15 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production';
 
-console.log(process.env.NODE_ENV);
-
-
 export const CLIENT_ID = isProd
-  ? 'dbf6e8904fe785430a96'
-  : '140ee27ef8df8ece846a';
+  ? '140ee27ef8df8ece846a'
+  : 'dbf6e8904fe785430a96';
 export const PROXY_URL = isProd
-  ? 'https://x-check-app.herokuapp.com/authenticate/'
-  : 'https://x-check-dev.herokuapp.com/authenticate';
+  ? ''
+  : 'https://x-check-app.herokuapp.com/authenticate/';
 
 export const JSON_API_URL = isProd
-  ? 'https://xcheck-server.herokuapp.com:3001'
+  ? 'https://xcheck-server.herokuapp.com'
   : 'http://localhost:3001';
