053c51a24dc677272669ff326884888cbf2ff0e2
Leonid Petrov <leonid.petrov@latoken.com>
2020-09-28 06:03:57 +0300

    fix: change Client id

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export const CLIENT_ID = isProd
-  ? '140ee27ef8df8ece846a'
+  ? '6fe549ee7be5faa1cf4c'
   : 'dbf6e8904fe785430a96';
 export const PROXY_URL = isProd
   ? 'https://x-check-gate.herokuapp.com/authenticate/'
