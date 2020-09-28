aef082b362e8ace51825b9373fdfd6a6423e0ed7
ellankz <enelipa@gmail.com>
2020-09-28 10:19:28 +0300

    fix: bring back eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,3 @@
 module.exports = {
   extends: ['airbnb-typescript-prettier'],
-  overrides: [
-    Object.assign(
-      {
-        files: ['**/*.test.js'],
-        env: { jest: true },
-        plugins: ['jest'],
-      },
-      require('eslint-plugin-jest').configs.recommended
-    ),
-  ],
 };
